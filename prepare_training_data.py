7d84187a919913f9f2e074c39669c0e12ce59c1d
Tran Cao Son 20215472 <caosonth2003@gmail.com>
2024-06-01 12:52:21 +0700

    split training data into 2 sub set in css10

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -2,13 +2,19 @@ with open('vin_bigdata/labels (1).txt','r',encoding='utf8') as f:
     data = f.read()
 alphabet = set()
 idx = 0
-with open('vin_bigdata/train.txt','w',encoding='utf-8') as f:
-    for l in data.split('\n'):
-        if len(l) < 3: continue
-        name_file, content = l.split('\t')
-        f.write(f'{idx:05d}|vietnamese|vietnamese|{name_file}|||{content}|\n')
-        idx += 1
-        alphabet.update(list(content))
+with open('vin_bigdata/val.txt','w',encoding='utf-8') as f_val:
+    with open('vin_bigdata/train.txt','w',encoding='utf-8') as f:
+        for l in data.split('\n'):
+            if len(l) < 3: continue
+            name_file, content = l.split('\t')
+            if idx < 54000:
+                
+                f.write(f'{idx:05d}|vietnamese|vietnamese|{name_file.replace("/content","")}|||{content}|\n')     
+            else:
+                f_val.write(f'{idx:05d}|vietnamese|vietnamese|{name_file.replace("/content","")}|||{content}|\n') 
+            idx += 1
+            alphabet.update(list(content))
+
 with open('vin_bigdata/alphabet.txt','w',encoding='utf-8') as f:
     f.write(''.join(sorted(list(alphabet))))
 
